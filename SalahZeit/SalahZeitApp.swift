2ac958bf0baee886ddaffbcb3e4d28ef8ad73044
Achmed06 <muhammed_y@outlook.de>
2026-09-26 18:14:37 +0200

    [wudu] Remove flaky extra simulator-only routes

--- SalahZeit/SalahZeitApp.swift
+++ SalahZeit/SalahZeitApp.swift
@@ -203,16 +203,12 @@ struct SalahPathApp: App {
             NavigationStack { WuduGuideView() }
         case "wudu-arm":
             NavigationStack { WuduGuideView(initialStepIndex: 6) }
-        case "wudu-left-arm":
-            NavigationStack { WuduGuideView(initialStepIndex: 7) }
         case "wudu-head":
             NavigationStack { WuduGuideView(initialStepIndex: 8) }
         case "wudu-ears":
             NavigationStack { WuduGuideView(initialStepIndex: 9) }
         case "wudu-foot":
             NavigationStack { WuduGuideView(initialStepIndex: 11) }
-        case "wudu-left-foot":
-            NavigationStack { WuduGuideView(initialStepIndex: 12) }
         case "ghusl":
             NavigationStack { GhuslGuideView() }
         case "tasbih":
